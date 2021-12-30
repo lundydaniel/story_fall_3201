54626483384d8907eea582aa471a519a45186460
Daniel Lundy <daniel.lundy@dcmail.ca>
2021-12-30 11:46:47 -0500

    Updated the repo. Simple console commands. Showing different features.

---
+++ script.js
@@ -0,0 +1,9 @@
+function connected (){
+    console.log("connected");
+};
+connected();
+
+function talkAboutCheese(cheeseType) {
+    console.log(`%c ${cheeseType} is my favorite this week.`,`color:orange;font-size: 30px`);
+}
+talkAboutCheese("Cheedar");
